766e66b28efadae925ffa6e4b4c875a9cfef130f
kazi.zahir <kazi.zahir@domain.com.au>
2017-02-13 14:08:58 +1100

    test

---
+++ TeamCityTest/Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityTest
+{
+    public class Tester
+    {
+        public string GetMessage()
+        {
+            return "Test is working";
+        }
+    }
+}
